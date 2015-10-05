7bd90a0eb5e9b51daeb96e892fba19ac17d51b86
vartan <michael.vartan@student.csulb.edu>
2015-10-04 19:36:18 -0700

    messed around with user accounts

---
+++ SESH_lite/client/templates/login/login.js
@@ -0,0 +1,36 @@
+
+Template.login.onCreated(() => {
+  this.badFormResponse = new ReactiveVar(undefined);
+});
+    let that = this;
+
+Template.login.events({
+  'keydown #loginForm input': function(event) {
+    that.badFormResponse.set(undefined);
+  },
+  'submit #loginForm': function(event) {
+    event.preventDefault();
+
+    var email = event.target.email.value;
+    var password = event.target.password.value;
+    var rawFormData = event.target;
+    Meteor.loginWithPassword(email, password, (err) => {
+      if (err) {
+        that.badFormResponse.set("Invalid username or password.");
+        // The user might not have been found, or their passwword
+        // could be incorrect. Inform the user that their
+        // login attempt has failed. 
+      } else {
+        Router.go("/profile");
+      }
+    });
+
+  }
+});
+
+Template.login.helpers({
+  formClass: () => {
+    return this.badFormResponse.get() ? "has-error" : "";
+  }, 
+  badFormResponse: () => this.badFormResponse.get()
+});
