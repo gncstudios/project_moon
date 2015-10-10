45252af9f4a2979f6b88e59ef9e1b8b4298a596e
Samuel Jacobs <samuel.jacobs@student.csulb.edu>
2015-10-10 10:05:13 -0700

    put in the file that michael emailed

--- SESH_lite/.meteor/local/build/programs/web.browser/app/client/templates/_shared/template.notFound.js
+++
@@ -1,8 +0,0 @@
-(function(){
-Template.__checkName("notFound");
-Template["notFound"] = new Template("Template.notFound", (function() {
-  var view = this;
-  return HTML.Raw('<div class="template-not-found">\n    <div class="container text-center">\n      <h1>Not Found</h1>\n      <p>\n        That page doesn\'t exist!\n      </p>\n    </div>\n  </div>');
-}));
-
-}).call(this);
